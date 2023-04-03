72ed1f81ebc96a4217c4e861a00b8370d8dc9e82
Tommy Pham <60743602+tommy-ph@users.noreply.github.com>
2023-04-03 14:39:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LogicalPractices
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Brute force: Time Complexity: O(n^2), Space complexity: O(1)
+            // ({ 2, 7, 11, 15 }, 9);
+            int[] resultOfTwoSum = TwoSum.ReturnTwoSum(new int[] { 2, 7, 11, 15 }, 9);
+
+            Console.WriteLine(string.Join(",",resultOfTwoSum));
+        }
+    }
+}
---
+++ TwoSum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPractices
+{
+    public class TwoSum
+    {
+        /* Description:
+            Given an array of integers, return indices of the two numbers such that they add
+            up to a specific target.
+            You may assume that each input would have exactly one solution, and you may not
+            use the same element twice.
+            Example:
+            Given nums = [2, 7, 11, 15], target = 9,
+            Because nums[0] + nums[1] = 2 + 7 = 9,
+            return [0, 1].
+         */
+        /// <summary>
+        /// Using a Dictionary: Time Complexity: O(n), Space complexity: O(n)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns>integer array of indices</returns>
+        public static int[] ReturnTwoSum(int[] nums, int target)
+        {
+            //Declarations
+            int arrayLength = nums.Length;
+            Dictionary <int, int> map = new Dictionary<int, int>();
+            //Validations
+            if(nums == null || arrayLength < 2)
+            {
+                return Array.Empty<int>();
+            }
+            //Logic
+            for(int i = 0; i < arrayLength; i++)
+            {
+                int firstNumber = nums[i];
+                int secondNumber = target - firstNumber;
+                if(map.TryGetValue(secondNumber, out int index))
+                {
+                    return new[] { index, i }; 
+                }
+                map[firstNumber] = i;
+            }
+            return Array.Empty<int>();
+        }
+    }
+}
